9179034312bcba46448570d48c22d5e71d3d810d
unknown <richard.vocoding101@gmail.com>
2018-12-24 18:46:28 -0500

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule} from '@angular/router';
+import { routes } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { ProbabilityCalculatorComponent } from './probability-calculator/probability-calculator.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProbabilityCalculatorComponent,
+    HomeComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
